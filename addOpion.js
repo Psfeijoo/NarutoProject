063797fa32e577251541a75cf2a880d1dba01012
DT3270 <34624867+DT3270@users.noreply.github.com>
2018-09-04 15:20:43 -0300

    Create addOpion.js

---
+++ addOpion.js
@@ -0,0 +1,5 @@
+/* Así agregamos registros a un desplegable en HTML
+document.write('<option value="01">La Caja<oprion/>')
+document.write('<option value="02">LATAM<oprion/>')
+document.write('<option value="03">Tu vieja<oprion/>')
+document.write('<option value="04">La vieja de Muhls<oprion/>')
