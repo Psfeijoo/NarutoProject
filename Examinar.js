2f6ee7dfc88709cadd57d672bca96f2311e1e370
Muhls <42978453+Muhls@users.noreply.github.com>
2018-09-04 15:34:56 -0300

    Add files via upload

---
+++ Examinar.js
@@ -0,0 +1,21 @@
+function leerArchivo(e) {
+    var archivo = e.target.files[0];
+    if (!archivo) {
+      return;
+    }
+    var lector = new FileReader();
+    lector.onload = function(e) {
+      var contenido = e.target.result;
+      mostrarContenido(contenido);
+    };
+    lector.readAsText(archivo);
+    console.log(contenido);
+}
+  
+  function mostrarContenido(contenido) {
+    var elemento = document.getElementById('contenido-archivo');
+    elemento.innerHTML = contenido;
+  }
+  
+  document.getElementById('file-input')
+    .addEventListener('change', leerArchivo, false);
