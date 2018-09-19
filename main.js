bdf2370b9717a7e7ab7cb87c1d7d85cf0e440e85
DT3270 <34624867+DT3270@users.noreply.github.com>
2018-09-19 14:54:44 -0300

    Actualización con el borrado de los objetos

---
+++ main.js
@@ -0,0 +1,17 @@
+const altoCelda = 22;
+const anchoCelda = 200;
+const columnas = 4;
+const anchoGrilla = anchoCelda * columnas + columnas -1;
+const margenTop = 200;
+const margenLeft = 8;
+
+var indPantalla = 'n'
+var lineaSalida = 1;
+var grillaSalida = [];
+var encabezado
+var paquetes = [];
+var productos = [];
+
+document.getElementById('file-input')
+  .addEventListener('change', procesarArchivo, false);
+
